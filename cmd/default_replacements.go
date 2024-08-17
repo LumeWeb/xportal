a34d72982f42aee47f12b6e07c41a2a9259103c4
Derrick Hammer <derrick@derrickhammer.com>
2024-08-17 17:23:37 -0400

    chore: update replacements

--- cmd/default_replacements.go
+++ cmd/default_replacements.go
@@ -17,14 +17,13 @@ func init() {
 		newVer string
 	}{
 		{"github.com/go-viper/mapstructure/v2", "v2.0.0", "github.com/LumeWeb/mapstructure/v2", "v2.0.0-20240603224933-c63fee0297e6"},
-		{"github.com/go-co-op/gocron/v2", "v2.9.0", "github.com/LumeWeb/gocron/v2", "v2.0.0-20240722160415-5b7bf7125d3a"},
-		{"github.com/go-co-op/gocron-redis-lock/v2", "v2.9.0", "github.com/LumeWeb/gocron-redis-lock/v2", "v2.0.0-20240722160415-5b7bf7125d3a"},
+		{"github.com/go-co-op/gocron/v2", "v2.9.0", "github.com/LumeWeb/gocron/v2", "v2.0.0-20240814201336-2d361739e9be"},
+		{"github.com/go-co-op/gocron-redis-lock/v2", "v2.0.1", "github.com/LumeWeb/gocron-redis-lock/v2", "v2.0.0-20240722104549-387206078839"},
 		{"github.com/gorilla/mux", "v1.8.1", "github.com/cornejong/gormux", "v0.0.0-20240526072501-ce1c97b033ec"},
 		{"github.com/tus/tusd-etcd3-locker", "v0.0.0-20200405122323-74aeca810256", "github.com/LumeWeb/tusd-etcd3-locker", "v0.0.0-20240510103936-0d66760cf053"},
 		{"github.com/tus/tusd/v2", "v2.4.0", "github.com/LumeWeb/tusd/v2", "v2.2.3-0.20240617010021-713280c42722"},
 		{"github.com/ugorji/go/codec", "v1.1.4", "github.com/ugorji/go/codec", "v1.2.7"},
 	}
-
 	// Loop through the list and create replacement rules
 	for _, repl := range replList {
 		defaultReplacements = append(defaultReplacements, xportal.NewReplace(

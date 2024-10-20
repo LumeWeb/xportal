99c100039896064082756e19968c1e527813847d
Derrick Hammer <derrick@derrickhammer.com>
2024-10-19 20:34:32 -0400

    chore: update dependencies

---
+++ .changeset/dry-jokes-enjoy.md
@@ -0,0 +1,5 @@
+---
+"@lumeweb/xportal": patch
+---
+
+update dependencies
--- cmd/default_replacements.go
+++ cmd/default_replacements.go
@@ -16,11 +16,10 @@ func init() {
 		newMod string
 		newVer string
 	}{
-		{"github.com/go-viper/mapstructure/v2", "v2.0.0", "github.com/LumeWeb/mapstructure/v2", "v2.0.0-20240603224933-c63fee0297e6"},
+		{"github.com/go-viper/mapstructure/v2", "v2.0.0", "github.com/LumeWeb/mapstructure/v2", "v2.0.0-20241020002627-300d9f77ec27"},
 		{"github.com/go-co-op/gocron/v2", "v2.9.0", "github.com/LumeWeb/gocron/v2", "v2.0.0-20240814201336-2d361739e9be"},
 		{"github.com/go-co-op/gocron-redis-lock/v2", "v2.0.1", "github.com/LumeWeb/gocron-redis-lock/v2", "v2.0.0-20240722104549-387206078839"},
-		{"github.com/gorilla/mux", "v1.8.1", "github.com/cornejong/gormux", "v0.0.0-20240526072501-ce1c97b033ec"},
-		{"github.com/tus/tusd-etcd3-locker", "v0.0.0-20200405122323-74aeca810256", "github.com/LumeWeb/tusd-etcd3-locker", "v0.0.0-20240510103936-0d66760cf053"},
+		{"github.com/gorilla/mux", "v1.8.1", "github.com/gorilla/mux", "v0.0.0-20240619235004-db9d1d0073d2"},
 		{"github.com/tus/tusd/v2", "v2.4.0", "github.com/LumeWeb/tusd/v2", "v2.2.3-0.20240617010021-713280c42722"},
 		{"github.com/ugorji/go/codec", "v1.1.4", "github.com/ugorji/go/codec", "v1.2.7"},
 	}

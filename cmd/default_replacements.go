a27585c0e67711f1f58db1f35e4d27a6f3a2ba8a
Derrick Hammer <derrick@derrickhammer.com>
2024-12-13 18:31:53 -0500

    chore: need to update mapstructure replacement and sync with dependency

--- cmd/default_replacements.go
+++ cmd/default_replacements.go
@@ -16,7 +16,7 @@ func init() {
 		newMod string
 		newVer string
 	}{
-		{"github.com/go-viper/mapstructure/v2", "v2.0.0", "github.com/LumeWeb/mapstructure/v2", "v2.0.0-20241213212524-92525f5828be"},
+		{"github.com/go-viper/mapstructure/v2", "v2.2.1", "github.com/LumeWeb/mapstructure/v2", "v2.0.0-20241213212524-92525f5828be"},
 		{"github.com/go-co-op/gocron/v2", "v2.9.0", "github.com/LumeWeb/gocron/v2", "v2.0.0-20240814201336-2d361739e9be"},
 		{"github.com/go-co-op/gocron-redis-lock/v2", "v2.0.1", "github.com/LumeWeb/gocron-redis-lock/v2", "v2.0.0-20240722104549-387206078839"},
 		{"github.com/gorilla/mux", "v1.8.1", "github.com/gorilla/mux", "v0.0.0-20240619235004-db9d1d0073d2"},

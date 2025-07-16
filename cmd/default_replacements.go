816d658b4bb503a445181f3d848de279eff8e3ca
Derrick Hammer <derrick@derrickhammer.com>
2025-07-16 07:07:54 -0400

    chore: simplify dependency replacements as we have removed the need for many

--- cmd/default_replacements.go
+++ cmd/default_replacements.go
@@ -16,14 +16,8 @@ func init() {
 		newMod string
 		newVer string
 	}{
-		{"github.com/go-viper/mapstructure/v2", "v2.2.1", "github.com/LumeWeb/mapstructure/v2", "v2.0.0-20241213212524-92525f5828be"},
-		{"github.com/go-co-op/gocron/v2", "v2.9.0", "github.com/LumeWeb/gocron/v2", "v2.0.0-20240814201336-2d361739e9be"},
-		{"github.com/go-co-op/gocron-redis-lock/v2", "v2.0.1", "github.com/LumeWeb/gocron-redis-lock/v2", "v2.0.0-20240722104549-387206078839"},
-		{"github.com/gorilla/mux", "v1.8.1", "github.com/gorilla/mux", "v0.0.0-20240619235004-db9d1d0073d2"},
-		{"github.com/tus/tusd/v2", "v2.4.0", "github.com/LumeWeb/tusd/v2", "v2.2.3-0.20241020013555-e29b4c6c01b7"},
-		{"github.com/ugorji/go/codec", "v1.1.4", "github.com/ugorji/go/codec", "v1.2.7"},
-		{"gorm.io/plugin/dbresolver", "v1.3.0", "gorm.io/plugin/dbresolver", "v1.5.3"},
 		{"git.apache.org/thrift.git", "v0.0.0-20180902110319-2566ecd5d999", "github.com/apache/thrift", "v0.0.0-20180902110319-2566ecd5d999"},
+		{"github.com/go-viper/mapstructure/v2", "v2.2.1", "github.com/LumeWeb/mapstructure/v2", "v2.0.0-20241213212524-92525f5828be"},
 	}
 	// Loop through the list and create replacement rules
 	for _, repl := range replList {
